6545656c36bdc401b4f3824563ba889f55a9d5f2
maryfranks <maryfranks@gmail.com>
2017-07-19 15:07:52 -0400

    adds route helpers to dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'pages#welcome'
+  root 'pages#welcome'
   get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
-  get '/kitten/:size' => 'pages#kitten'
-  get '/many_kittens/:size' => 'pages#kittens'
-  get '/secrets/:magic_word' => 'pages#secrets'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+  get '/many_kittens/:size' => 'pages#kittens', as: 'many_kittens'
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 end
