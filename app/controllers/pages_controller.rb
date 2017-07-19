439df1e97d4428b12f5b86161f464d53975a2196
maryfranks <maryfranks@gmail.com>
2017-07-19 14:39:53 -0400

    adds dynamic route and method for kitten

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,10 @@ class PagesController < ApplicationController
     @header = "Enter our Contest"
   end
 
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+
 end
